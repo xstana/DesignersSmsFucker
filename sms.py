6ed5893555bdc41be11e2ca7d36e538ff3315913
Orkun ONUK <info@orkunonuk.com>
2023-12-09 16:35:21 +0300

    api Update sms.py
    api update

--- sms.py
+++ sms.py
@@ -66,21 +66,6 @@ class SendSms():
         except:
             print(f"{Fore.LIGHTRED_EX}[-] {Style.RESET_ALL}Başarısız! {self.phone} --> bim.veesk.net")
 
-
-    #englishhome.com
-    def Englishhome(self):
-        try:
-            data = {"first_name": "Memati", "last_name": "Bas", "email": self.mail, "phone": f"0{self.phone}", "password": "31ABC..abc31", "email_allowed": "true", "sms_allowed": "true", "confirm": "true", "tom_pay_allowed": "true"}
-            home = requests.post("https://www.englishhome.com:443/enh_app/users/registration/", data=data)
-            if home.status_code == 202:
-                print(f"{Fore.LIGHTGREEN_EX}[+] {Style.RESET_ALL}Başarılı! {self.phone} --> englishhome.com")
-                self.adet += 1
-            else:
-                raise
-        except:
-            print(f"{Fore.LIGHTRED_EX}[-] {Style.RESET_ALL}Başarısız! {self.phone} --> englishhome.com")
-    
-
     #mopas.com.tr
     def Mopas(self):
         try:
